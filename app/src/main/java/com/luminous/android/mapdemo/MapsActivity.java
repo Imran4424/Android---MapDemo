b76c9dc289f800418b4292a10958bed4c32df943
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-09-15 08:47:46 +0600

    main activity

--- app/src/main/java/com/luminous/android/mapdemo/MapsActivity.java
+++ app/src/main/java/com/luminous/android/mapdemo/MapsActivity.java
@@ -8,6 +8,7 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
@@ -40,7 +41,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         // Add a marker in Sydney and move the camera
         LatLng rangpurZillaSchool = new LatLng(25.7574106,89.2406631);
-        mMap.addMarker(new MarkerOptions().position(rangpurZillaSchool).title("Best school, Ancient one"));
+        mMap.addMarker(new MarkerOptions().position(rangpurZillaSchool).title("Best school, Ancient one").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(rangpurZillaSchool));
     }
 }
