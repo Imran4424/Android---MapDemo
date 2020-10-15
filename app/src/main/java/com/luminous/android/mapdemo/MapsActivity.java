2fe1cd18b2d6601eff697fb1a3e6800cffe83a0f
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-10-15 22:47:44 +0600

    map and location demo

--- app/src/main/java/com/luminous/android/mapdemo/MapsActivity.java
+++ app/src/main/java/com/luminous/android/mapdemo/MapsActivity.java
@@ -8,12 +8,14 @@ import androidx.fragment.app.FragmentActivity;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,6 +27,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -82,7 +85,11 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                 Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
                 try {
-                    geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                    List<Address> listAddress = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                    if (listAddress != null && listAddress.size() > 0) {
+                        Log.i("Place Info", listAddress.get(0).toString());
+                    }
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
